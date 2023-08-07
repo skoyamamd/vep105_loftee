9db72177a7e89403e9ef3c465f68218c465b62d6
Barney Hill <barneyhill1@gmail.com>
2023-08-07 10:33:04 +0100

    extract homo_sapiens

--- download_data_dx.sh
+++ download_data_dx.sh
@@ -4,3 +4,4 @@ unset DX_WORKSPACE_ID; \
 dx cd $DX_PROJECT_CONTEXT_ID:; \
 mkdir vep_data/
 dx download /annotation/data/* -o vep_data/
+tar xzf vep_data/homo_sapiens_vep_110_GRCh38.tar.gz -C vep_data
