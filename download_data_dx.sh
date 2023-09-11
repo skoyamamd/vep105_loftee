fdd41d31908973e753461bb8f7308a82050240d5
Barney Hill <barneyh@NDPH9082.local>
2023-09-11 16:36:57 +0100

    Remove download_data_dx.sh

--- download_data_dx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-unset DX_WORKSPACE_ID; \
-dx cd $DX_PROJECT_CONTEXT_ID:; \
-mkdir vep_data/
-dx download /annotation/data/* -o vep_data/
-tar xzf vep_data/homo_sapiens_vep_105_grch38.tar.gz -C vep_data
