740aaecc3862c5c99b9d1a58475740814dd50261
Barney Hill <barneyh@NDPH9082.local>
2023-09-13 11:44:35 +0100

    add logs + decompress homo_sapiens

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Download VEP cache
+echo "Downloading VEP cache..."
+
 curl -SL https://ftp.ensembl.org/pub/release-105/variation/indexed_vep_cache/homo_sapiens_vep_105_GRCh38.tar.gz -o vep_data/homo_sapiens_vep_105_GRCh38.tar.gz
+tar -xvzf vep_data/homo_sapiens_vep_105_GRCh38.tar.gz -C vep_data/
 
 # Download necessary files for LOFTEE
+echo "Downloading LOFTEE files..."
+
 mkdir -p vep_data && \
 curl -SL https://personal.broadinstitute.org/konradk/loftee_data/GRCh38/loftee.sql.gz -o vep_data/loftee.sql.gz && \
 curl -SL https://personal.broadinstitute.org/konradk/loftee_data/GRCh38/human_ancestor.fa.gz -o vep_data/human_ancestor.fa.gz && \
@@ -12,4 +17,5 @@ curl -SL https://personal.broadinstitute.org/konradk/loftee_data/GRCh38/human_an
 curl -SL https://personal.broadinstitute.org/konradk/loftee_data/GRCh38/gerp_conservation_scores.homo_sapiens.GRCh38.bw -o vep_data/gerp_conservation_scores.homo_sapiens.GRCh38.bw
 
 # Download dbNSFP
+echo "Downloading dbNSFP cache..."
 curl -SL https://dbnsfp.s3.amazonaws.com/dbNSFP4.3a.zip -o vep_data/dbNSFP4.3a.zip
